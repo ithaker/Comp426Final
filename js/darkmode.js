12baa0a8a2b62a7532f5c5d6b643c077451e846c
Ishan Thaker <ishanthaker@gmail.com>
2021-08-11 23:21:05 -0400

    Implemented high contrast mode

---
+++ js/darkmode.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector(".toggle");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
+    document.body.classList.add("dark-mode");
+}
+
+btn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    let theme = "light";
+    if (document.body.classList.contains("dark-mode")) {
+        theme = "dark"
+    }
+    localStorage.setItem("theme", theme);
+})
